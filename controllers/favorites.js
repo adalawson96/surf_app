3e9f8af9edd16a2be484c1991d9727cb551cf160
adalawson96 <adalawson96@gmail.com>
2023-03-06 16:50:11 +0100

    edit favorite

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -7,18 +7,17 @@ module.exports = {
 };
 
 async function index(req, res) {
-   user = await User.findById(req.user._id) 
+   let user = await User.findById(req.user._id) 
    .populate('favorites')
    .exec()
         res.render('favorites/index', { title: 'Favorites', favorites: user.favorites });
 };
 
-
 async function update(req, res) {
-    user = await User.findOne(req.user._id)
+    let user = await User.findOne(req.user._id)
     if (!user.favorites.includes(req.params.id)) {
         user.favorites.push(req.params.id)
        await user.save()
     };
-    res.render('favorites/index', { title: 'Favorites'})
+    res.render('favorites/index', { title: 'Favorites', favorites: user.favorites });
 };
